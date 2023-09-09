8687b66d998b8d5c069f8930259b9664b53e2dd1
andika <zoneandika@gmail.com>
2023-09-09 19:11:06 +0700

    initial commit

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Image from "next/legacy/image";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src="/humberger.svg" className="w-[50px] bg-red-500" />
+    </nav>
+  );
+}
