9b59f5ec276b913e2c8159d439be29fc8027cee2
andika <zoneandika@gmail.com>
2023-09-17 22:48:36 +0700

    map switch fixed

--- src/app/main/map.jsx
+++ src/app/main/map.jsx
@@ -2,43 +2,58 @@
 
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+var overlay = [];
 
-const Map = () => {
+const Map = (e) => {
+  const [map, setMap] = useState(undefined);
+
+  const selectedMap = "map/" + e.selectedMap + ".png";
   const locationMap = useRef();
+  var bounds = [
+    [-26.5, -25],
+    [1021.5, 1023],
+  ];
+
+  // init map
   useEffect(() => {
-    const southWest = L.latLng(-26, -25); // Koordinat sudut barat daya (latitude, longitude)
-    const northEast = L.latLng(1025.5, 1023); // Koordinat sudut timur laut (latitude, longitude)
-    const batas = L.latLngBounds(southWest, northEast);
+    console.log("map created");
+    var southWest = L.latLng(-26, -25);
+    var northEast = L.latLng(1025.5, 1023);
+
     const map = L.map(locationMap.current, {
       crs: L.CRS.Simple,
       maxZoom: 2,
       minZoom: 0,
-      maxBounds: batas,
+      maxBounds: L.latLngBounds(southWest, northEast),
       maxBoundsViscosity: 4.0,
-    });
-    const bounds = [
-      [-26.5, -25],
-      [1021.5, 1023],
-    ];
-    const mapOverlay = L.imageOverlay("map/ascent.png", bounds).addTo(map);
-    map.fitBounds(bounds);
-
-    const getBounds = mapOverlay.getBounds();
-    console.log(getBounds);
-    // pin section
-    L.marker([-26, -25], {
-      icon: L.icon({
-        iconUrl: "image/displayicon 1.png",
-        iconSize: [30, 30],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        tooltipAnchor: [16, -28],
-        shadowSize: [41, 41],
-      }),
-    }).addTo(map);
+    }).fitBounds(bounds);
+    setMap(map);
   }, []);
-  return <div id="map" ref={locationMap} className="w-full h-full"></div>;
+
+  if (map !== undefined) {
+    let newMap = L.imageOverlay(selectedMap, bounds, { myLayerId: 1 }).addTo(
+      map
+    );
+    overlay.push(newMap);
+  }
+
+  console.log(overlay);
+  if (overlay[overlay.length - 2] !== undefined) {
+    overlay[overlay.length - 2].remove();
+  }
+  console.log(overlay[overlay.length - 2]);
+  useEffect(() => {
+    // L.imageOverlay(selectedMap, bounds).addTo(map);
+  }, [e]);
+
+  return (
+    <div
+      id="map"
+      ref={locationMap}
+      className="w-full h-full !bg-[#16161A]"
+    ></div>
+  );
 };
 
 export default Map;
--- src/app/main/page.jsx
+++ src/app/main/page.jsx
@@ -2,6 +2,7 @@
 
 import dynamic from "next/dynamic";
 import { Poppins, Inter } from "next/font/google";
+import { useState } from "react";
 const Map = dynamic(() => import("./map"), { ssr: false });
 
 const poppins = Poppins({
@@ -16,12 +17,19 @@ const inter = Inter({
 });
 
 export default function page() {
+  const [map, setMap] = useState("ascent");
+  const [agent, setAgent] = useState("sova");
+
+  const selectMapHandle = (e) => {
+    setMap(e.target.value);
+  };
+
   return (
     <div className="py-[100px] w-full relative">
       <div
         className={`w-[90vw] h-[90vw] md:w-[70vw] md:h-[70vw] lg:w-[40vw] lg:h-[40vw] mx-auto relative cursor-move`}
       >
-        <Map />
+        <Map selectedMap={map} />
       </div>
       <div className="flex flex-wrap gap-[5px] py-[90px] w-[70%] mx-auto justify-center">
         <img src="/agent/astra/astra.svg" className="w-[60px]" alt="" />
@@ -48,8 +56,11 @@ export default function page() {
         <img src="/agent/yoru/yoru.svg" className="w-[60px]" alt="" />
       </div>
 
+      {/* display data */}
       <div className="absolute top-[50px] left-[50px] text-white">
-        <h1 className={`${poppins.className} text-[2rem]`}>ASCENT</h1>
+        <h1 className={`${poppins.className} text-[2rem]`}>
+          {map.toUpperCase()}
+        </h1>
         <div
           className={`px-[10px] py-[5px] flex gap-[20px] border-solid border-white border-[1px] w-fit ${inter.className} text-[.8rem]`}
         >
@@ -86,6 +97,26 @@ export default function page() {
             />
           </button>
         </div>
+      </div>
+
+      <div className="absolute top-[50px] right-[50px]">
+        <select
+          name="maps"
+          id="selectMap"
+          className={`${inter.className} rounded-[5px] `}
+          onChange={(e) => selectMapHandle(e)}
+        >
+          <option value="ascent">ascent</option>
+          <option value="bind">bind</option>
+          <option value="breeze">breeze</option>
+          <option value="fracture">fracture</option>
+          <option value="haven">haven</option>
+          <option value="icebox">icebox</option>
+          <option value="lotus">lotus</option>
+          <option value="pearl">pearl</option>
+          <option value="split">split</option>
+          <option value="sunset">sunset</option>
+        </select>
       </div>
     </div>
   );
