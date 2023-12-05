134349f47aa94c3f47206fd6fe8a28103f77853a
andika <zoneandika@gmail.com>
2023-12-05 20:32:59 +0700

    progres edit profile feature -backend

---
+++ src/app/api/user/edit/route.js
@@ -0,0 +1,30 @@
+export const dynamic = "static";
+import { NextResponse } from "next/server";
+import connectMongoDB from "../../../../../libs/mongodb";
+import User from "../../../../../models/user";
+import bcrypt from "bcryptjs";
+
+export async function POST(request) {
+  await connectMongoDB();
+  try {
+    const { username, email, password, deskripsi, pp, tag, id } =
+      await request.json();
+
+    // middlewere
+    if (
+      username !== undefined ||
+      email !== undefined ||
+      password !== undefined ||
+      tag !== undefined
+    ) {
+    }
+
+    await User.updateOne({ _id: id }, { $set: { deskripsi, username, pp } });
+    return NextResponse.json(
+      { message: "berhasil mengupdate data" },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
+}
