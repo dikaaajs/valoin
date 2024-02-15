299058e4ef20472ab7043c899d27876f6706aeb8
andika <zoneandika@gmail.com>
2024-02-15 20:02:44 +0700

    finished create page search person (api and client)

--- src/app/api/user/getAll/route.js
+++ src/app/api/user/getAll/route.js
@@ -3,7 +3,8 @@ import connectMongoDB from "../../../../../libs/mongodb";
 import User from "../../../../../models/user";
 
 export async function POST(req) {
-  const limit = 10;
+  let nextQuery = true;
+  const limit = 11;
   let { page, username } = await req.json();
   if (page === undefined) {
     page = 1;
@@ -11,11 +12,26 @@ export async function POST(req) {
   await connectMongoDB();
 
   try {
-    const users = await User.find()
-      .select("-password")
-      .limit(limit)
-      .skip((page - 1) * limit);
-    return NextResponse.json({ users }, { status: 200 });
+    let users;
+    if (username !== undefined) {
+      users = await User.find({ username: new RegExp(username, "i") })
+        .select("-password")
+        .limit(limit)
+        .skip((page - 1) * 10);
+    } else {
+      users = await User.find()
+        .select("-password")
+        .limit(limit)
+        .skip((page - 1) * 10);
+    }
+
+    if (users[10] === undefined) {
+      nextQuery = false;
+    } else {
+      users.pop();
+    }
+
+    return NextResponse.json({ users, nextQuery }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ err: error.message }, { status: 500 });
   }
--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -4,66 +4,136 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Loading from "../components/Loading";
 import Link from "next/link";
+import { Tooltip } from "antd";
+
+const checkRole = (tags) => {
+  return (
+    <div className="flex gap-[5px]">
+      {/* admin role */}
+      {tags.includes("admin") && (
+        <Tooltip title="admin" color="#a855f7">
+          <p>📌</p>
+        </Tooltip>
+      )}
+
+      {/* developer role */}
+      {tags.includes("dev") && (
+        <Tooltip title="developer" color="#a855f7">
+          <p>🛠</p>
+        </Tooltip>
+      )}
+    </div>
+  );
+};
 
 export default function page() {
   const [page, setPage] = useState(1);
-  const [users, setUsers] = useState(null);
+  const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
   const [filterUsername, setFilterUsername] = useState(null);
+  const [nextQuery, setNextQuery] = useState(true);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(filterUsername);
+    const response = await axios.post("/api/user/getAll", {
+      username: filterUsername,
+    });
+    setUsers(response.data.users);
   };
 
   const getData = async () => {
     const response = await axios.post("/api/user/getAll", {
       page,
     });
-    setUsers(response.data.users);
+    setUsers([...users, ...response.data.users]);
+    setNextQuery(response.data.nextQuery);
     setLoading(false);
   };
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [page]);
 
   if (loading) return <Loading />;
 
   return (
-    <div className="py-[40px] px-[15px]">
+    <div className="py-[40px] px-[15px] min-h-screen">
       {/* search bar */}
-      <div className="pb-[20px] ">
-        <form className="flex justify-between" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            className="rounded-[9px] w-[80%] bg-transparent text-white"
-            placeholder="username ..."
-            onChange={(e) => setFilterUsername(e.target.value)}
-          />
-          <button className="" type="submit">
-            <img src="/search.svg" className="w-[40px]" alt="" />
-          </button>
+      <div className="pb-[20px] md:py-[50px]">
+        {/* judul */}
+        <div>
+          <h1 className="font-poppins-bold text-[2.5rem] text-white uppercase relative py-[15px] w-fit mx-auto">
+            <img
+              src="/svg/balok.svg"
+              className="w-[50px] absolute -z-30 -left-[20px] top-0"
+              alt=""
+            />{" "}
+            cari orang
+          </h1>
+        </div>
+
+        <form onSubmit={handleSubmit} className="relative">
+          <div className="relative w-[95%] md:w-[80%] mx-auto h-[40px]">
+            <input
+              type="text"
+              className="rounded-[5px] bg-transparent text-white w-full h-full pl-[50px]"
+              placeholder="username ..."
+              onChange={(e) => setFilterUsername(e.target.value)}
+            />
+            <button
+              className="opacity-50 absolute w-[25px] top-[8px] left-[10px]"
+              type="submit"
+            >
+              <img src="/search.svg" className="w-full" alt="" />
+            </button>
+          </div>
         </form>
       </div>
 
       {/* user */}
-      <div className="flex flex-col md:flex-wrap gap-[20px]">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-[25px] md:px-[25px]">
         {users.map((i, idx) => {
           return (
-            <div key={idx} className="text-white flex gap-[10px] items-center">
-              <div className="w-fit">
-                <img src={i.pp} className="w-[30px] rounded-full" />
+            <Link
+              href={`/profile/${i.username}`}
+              key={idx}
+              className="text-white flex gap-[10px] items-center w-full border-[1px] border-white rounded-[5px] py-[3px] px-[10px] relative"
+            >
+              <img
+                src="/blob2.svg"
+                className="absolute bottom-[-20px] left-[-20px] w-[60px] z-10 rotate-180"
+                alt=""
+              />
+              <div className="w-fit z-50">
+                <img src={i.pp} className="w-[30px] rounded-full z-50" />
               </div>
               <div>
-                <Link href={`/profile/${i.username}`} className="underline">
-                  <p>{i.username}</p>
-                </Link>
+                <span className="flex gap-[5px]">
+                  <p className="underline">{i.username}</p>
+                  {checkRole(i.tag)}
+                </span>
                 <p className="text-[.7rem]">{i.deskripsi}</p>
               </div>
-            </div>
+            </Link>
           );
         })}
+      </div>
+
+      {/* view more buttton */}
+      <div>
+        <button
+          className={`btn rounded-[5px] !bg-blue-400 !text-white mx-auto block my-[30px] relative ${
+            nextQuery ? "" : "hidden"
+          }`}
+          onClick={() => setPage(page + 1)}
+        >
+          view more
+          <img
+            src="/svg/bata.svg"
+            className="w-[40px] absolute right-[-25px] top-[-10px] -z-20"
+            alt=""
+          />
+        </button>
       </div>
     </div>
   );
